09a5559c14e48c0df9346ba818f40abaf910fe25
Slowwie <michaelbrauner82@gmail.com>
2021-07-25 16:50:37 +0200

    Extracted route loader to braunstetter/plugin-bundle

--- src/Controller/CpController.php
+++ src/Controller/CpController.php
@@ -4,15 +4,13 @@
 namespace Rock\Controller;
 
 
-use Braunstetter\PluginBundle\Services\Plugins;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class CpController extends AbstractController
 {
-    public function index(Plugins $plugins): Response
+    public function index(): Response
     {
-        dump($plugins->findAll());
         return $this->render('@Rock/cp/dashboard.html.twig');
     }
 }

14e04ad5395652dc8d6fb0845ca3db0e694bba4d
Slowwie <michaelbrauner82@gmail.com>
2021-07-25 15:14:14 +0200

    Extracted plugin system into own repository braunstetter/plugin-bundle

--- src/Controller/CpController.php
+++ src/Controller/CpController.php
@@ -4,13 +4,15 @@
 namespace Rock\Controller;
 
 
+use Braunstetter\PluginBundle\Services\Plugins;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class CpController extends AbstractController
 {
-    public function index(): Response
+    public function index(Plugins $plugins): Response
     {
+        dump($plugins->findAll());
         return $this->render('@Rock/cp/dashboard.html.twig');
     }
 }
